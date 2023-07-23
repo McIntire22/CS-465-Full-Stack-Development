6eba70599ad7d839cbe8ac03c33d5b567fd3f178
Jordyn McIntire <McIntiresmasonry@yahoo.com>
2023-07-23 16:22:05 -0400

    Express website, static HTML only

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -2,5 +2,5 @@ module.exports = {
     modelBaseDirectory: 'app_server/database/models', // model directory name
     models: ["*.js", "!db.js"], // model matcher
     data: 'data', // data directory name
-    db: 'mongodb://localhost:27017/travlr' // db connection url
+    db: 'mongodb://localhost:127.0.0.1/travlr' // db connection url
   };
